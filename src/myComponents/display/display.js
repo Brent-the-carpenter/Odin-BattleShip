2d0578971c930dd3784ee8ea3946d2a159450fbe
Brent-the-carpenter <beh337@gmail.com>
2024-03-08 22:28:33 -0500

    got the dislay workig up to player1 vs ai player choosing ship location.

--- src/myComponents/display/display.js
+++ src/myComponents/display/display.js
@@ -1,3 +1,13 @@
+const newGame = document.querySelector("#newGame");
+const body = document.querySelector("body");
+const gatherPlayer1Info = document.querySelector("#gatherPlayer1Info");
+const StartGame = document.querySelector("#startGame");
+const opponentContainer = document.querySelector(".opponentContainer");
+const gatherPlayer2Info = document.querySelector("#gatherPlayer2Info");
+const vsAi = document.querySelector("#vs");
+const greeting = document.querySelector("#greeting");
+const boardContainer = document.querySelector(".boardContainer");
+
 function renderGameBoard(playerBoardState, containerId) {
   console.log(containerId);
   const container = document.getElementById(containerId);
@@ -22,32 +32,68 @@ function renderGameBoard(playerBoardState, containerId) {
 }
 
 function renderGetPlayer1() {
-  console.log("running");
-  const newGame = document.querySelector("#newGame");
-  const body = document.querySelector("body");
-  const gatherPlayer1Info = document.querySelector("#gatherPlayer1Info");
   newGame.style.display = "none";
   gatherPlayer1Info.style.display = "block";
   body.appendChild(gatherPlayer1Info);
 }
 function renderGetPlayer2() {
-  const body = document.querySelector("body");
-  const StartGame = document.querySelector("#startGame");
-  const opponentContainer = document.querySelector(".opponentContainer");
-  const gatherPlayer2Info = document.querySelector("#gatherPlayer2Info");
   opponentContainer.style.display = "none";
   gatherPlayer2Info.style.display = "block";
   StartGame.style.display = "block";
 }
 
 function renderVsAi() {
-  const body = document.querySelector("body");
-  const StartGame = document.querySelector("#startGame");
-  const opponentContainer = document.querySelector(".opponentContainer");
-  const vsAi = document.querySelector("#vs");
   opponentContainer.style.display = "none";
   vsAi.style.display = "block";
   StartGame.style.display = "block";
 }
 
-export { renderGameBoard, renderGetPlayer1, renderGetPlayer2, renderVsAi };
+function clearScreen() {
+  StartGame.style.display = "none";
+  opponentContainer.style.display = "none";
+  vsAi.style.display = "none";
+  gatherPlayer1Info.style.display = "none";
+  greeting.style.display = "none";
+}
+
+function placeYourShipsMessage(player) {
+  const div = document.createElement("div");
+  div.setAttribute("id", "placeShipMessage");
+  const placeShipMessage = `${player} place your ships`;
+  div.textContent = placeShipMessage;
+  const letterContainer = document.createElement("div");
+  letterContainer.setAttribute("id", "letterContainer");
+  const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  letters.forEach((value) => {
+    const letter = document.createElement("div");
+    letter.classList.add("letter");
+    letter.textContent = value;
+    letterContainer.appendChild(letter);
+  });
+  const numberContainer = document.createElement("div");
+  numberContainer.setAttribute("id", "numberContainer");
+  const numbers = ["", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  numbers.forEach((value) => {
+    const number = document.createElement("div");
+    number.classList.add("number");
+    number.textContent = value;
+    numberContainer.appendChild(number);
+  });
+  const readyButton = document.createElement("button");
+  readyButton.textContent = "Ready";
+  readyButton.setAttribute("id", "readyButton");
+  body.prepend(div);
+  div.append(numberContainer);
+  body.appendChild(readyButton);
+  boardContainer.append(letterContainer);
+  boardContainer.style.display = "flex";
+}
+
+export {
+  renderGameBoard,
+  renderGetPlayer1,
+  renderGetPlayer2,
+  renderVsAi,
+  clearScreen,
+  placeYourShipsMessage,
+};
--- src/myComponents/gameLoop.js
+++ src/myComponents/gameLoop.js
@@ -1,15 +1,36 @@
 import Player from "./classes/playerClass";
 import {
+  renderGameBoard,
   renderGetPlayer1,
   renderGetPlayer2,
   renderVsAi,
+  clearScreen,
+  placeYourShipsMessage,
 } from "./display/display";
 
 const newGame = document.querySelector("#newGame");
-const Player1Name = document.querySelector("#player1").value;
+const Player1Name = document.querySelector("#player1");
+const Player2Name = document.querySelector("#player2");
 const getPlayer2Name = document.querySelector("#player2Button");
 const AiButton = document.querySelector("#AI");
+const startButton = document.querySelector("#startGame");
+
+function opponentCheck() {
+  if (Player2Name !== "") return Player2Name.value;
+  return "AI";
+}
+function startGame() {
+  const player1 = new Player(Player1Name.value);
+  const player2 = new Player(opponentCheck);
+  if (player2.name === "AI") {
+    player2.computer = true;
+  }
+  clearScreen();
+  placeYourShipsMessage(player1.name);
+  renderGameBoard(player1.gameBoard.gameBoard, "gameBoard");
+}
 
 newGame.addEventListener("click", () => renderGetPlayer1());
 getPlayer2Name.addEventListener("click", () => renderGetPlayer2());
 AiButton.addEventListener("click", () => renderVsAi());
+startButton.addEventListener("click", () => startGame());
